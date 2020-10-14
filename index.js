9c06974684785f629cd6a717d1e478fe6ad91b96
chandrawardhana01 <ugmbackend01@gmail.com>
2020-10-15 00:41:30 +0700

    connect to MonggoDB

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 import express from 'express';
 import morgan from  'morgan';
+import mongoose from 'mongoose';
 
 const app = express();
+
+//connect to mongoDB
+mongoose.connect('mongodb+srv://admin:<kodeamanterbaik>@homework.hin5z.mongodb.net/<jadwalin>?retryWrites=true&w=majority',
+{
+    userNewUrlParser: true,
+    useUnifiedTopology: true,
+}, () => {
+    console.log('Connect to database success');
+});
+
 
 //Middlewares
 app.use(express.json());
