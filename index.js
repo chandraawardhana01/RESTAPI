ccca83b70a2da4c81c7513aa6706bbdec377f607
chandrawardhana01 <ugmbackend01@gmail.com>
2020-10-15 00:29:21 +0700

    Mengimplementasikan middleware

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-import express from 'express'
+import express from 'express';
+import morgan from  'morgan';
+
 const app = express();
 
+//Middlewares
+app.use(express.json());
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
     res.json({
